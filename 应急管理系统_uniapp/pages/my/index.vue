ff50ddcf708e21271ed56d4ca2ddcbf60806ed59
zej574099903 <zhouenjun@163.com>
2024-12-20 13:24:33 +0800

    feat: 搭建基础框架

---
+++ 应急管理系统_uniapp/pages/my/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="my-container">
+    <view class="user-info">
+      <text class="title">个人信息</text>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+.my-container {
+  padding: 20rpx;
+}
+.user-info {
+  background-color: #fff;
+  padding: 30rpx;
+  border-radius: 12rpx;
+}
+.title {
+  font-size: 32rpx;
+  font-weight: bold;
+}
+</style>
